143234122780f7b2e441c0f768b6cb46e87541c1
Robert Carosi <robert@carosi.nl>
2025-02-05 16:48:34 +0100

    Update rmapi binaries to v0.0.29

--- update_rmapi_binaries.sh
+++ update_rmapi_binaries.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+# $1: The tag to update to, or latest if undefined
+
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 git clone git@github.com:ddvk/rmapi.git
 cd rmapi || exit
 git fetch
-git checkout "$(git tag --sort=-creatordate | head -n 1)"
+git checkout "${1:-$(git tag --sort=-creatordate | head -n 1)}"
 
 move() {
   mv rmapi "${SCRIPT_DIR}"/src/main/jib/usr/local/bin/rmapi_"${1}"
@@ -14,6 +16,7 @@ move() {
 GOOS=linux GOARCH=arm64 go build && move "aarch64"
 GOOS=linux GOARCH=arm GOARM=5 go build && move "arm"
 GOOS=linux GOARCH=amd64 go build && move "amd64"
+GOOS=darwin GOARCH=arm64 go build && move "arm64"
 
 cd - || exit
 rm -rf rmapi
